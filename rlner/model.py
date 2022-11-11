453d0fffabbcee9b51c39012f44d3c7032d3e056
enyquist <erik.nyquist@comcast.net>
2022-11-11 13:24:26 -0500

    Updated bi-lstm model to return sequences

--- rlner/model.py
+++ rlner/model.py
@@ -1,13 +1,13 @@
 # standard libaries
 import io
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Tuple
 
 # third party libraries
 import numpy as np
+import tensorflow.keras.layers as layers
+import tensorflow.keras.models as models
 from crf import CRF
-from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Embedding, TimeDistributed
-from tensorflow.keras.models import Sequential
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = ROOT_DIR / "data"
@@ -16,8 +16,16 @@ EMBEDDING_DIR = DATA_DIR / "embeddings"
 
 def create_model(
     vocab_size: int, max_length: int, embedding_dim: int, word_index: Dict[str, int], tag_index: Dict[str, int]
-) -> Sequential:
-    """Create Bi-LSTM CRF model in tensorflow
+) -> Tuple[models.Model]:
+    """Create Bi-LSTM CRF model in tensorflow.
+
+    Model1 is the trainable model. Model2 is for predictions and returns:
+    [predicted labels, LSTM hidden state (Forward and backward), LSTM cell state (forward and backward), embeddings]
+
+    This is leveraged to build the REINFORCE states.
+
+    Adapted from:
+    https://github.com/ngoquanghuy99/POS-Tagging-BiLSTM-CRF
 
     Args:
         vocab_size (int): Size of vocabulary
@@ -27,7 +35,8 @@ def create_model(
         tag_index (Dict[str, int]): Index mapping tokens to ints
 
     Returns:
-        Sequential: Compiled Sequential Model
+        Tuple[Model]: Compiled Model and Non-compiled Model
+        with exposed LSTM and embedding layers
     """
 
     embeddings_index = {}
@@ -45,25 +54,29 @@ def create_model(
             if embedding_vector is not None:
                 embeddings_matrix[i] = embedding_vector
 
-    model = Sequential()
-    model.add(
-        Embedding(
-            input_dim=vocab_size,
-            output_dim=embedding_dim,
-            input_length=max_length,
-            weights=[embeddings_matrix],
-            mask_zero=True,
-        )
-    )
+    inputs = layers.Input(shape=(max_length,))
 
-    model.add(Bidirectional(LSTM(units=embedding_dim, return_sequences=True, recurrent_dropout=0.01)))
+    embeddings = layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_dim,
+        input_length=max_length,
+        weights=[embeddings_matrix],
+        mask_zero=True,
+    )(inputs)
 
-    model.add(TimeDistributed(Dense(len(tag_index))))
+    lstm_out, sh_fw, sc_fw, sh_bw, sc_bw = layers.Bidirectional(
+        layers.LSTM(units=embedding_dim, return_sequences=True, return_state=True, recurrent_dropout=0.01)
+    )(embeddings)
 
-    crf = CRF(len(tag_index), sparse_target=True)
-    model.add(crf)
+    time_dist = layers.TimeDistributed(layers.Dense(len(tag_index)))(lstm_out)
 
-    model.compile(optimizer="adam", loss=crf.loss, metrics=["crf.accuracy"])
-    model.summary()
+    crf = CRF(len(tag_index), sparse_target=False)
+    pred = crf(time_dist)
 
-    return model
+    model1 = models.Model(inputs=[inputs], outputs=[pred])
+    model2 = models.Model(inputs=[inputs], outputs=[pred, sh_fw, sc_fw, sh_bw, sc_bw, embeddings])
+
+    model1.compile(optimizer="adam", loss=crf.loss, metrics=[crf.accuracy])
+    model1.summary()
+
+    return model1, model2
