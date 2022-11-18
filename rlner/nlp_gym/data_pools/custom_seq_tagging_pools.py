a2df5cb6e619f83e76f39d0eb96f16acc207b300
enyquist <erik.nyquist@comcast.net>
2022-11-18 11:08:41 -0500

    Re3d data pool

--- rlner/nlp_gym/data_pools/custom_seq_tagging_pools.py
+++ rlner/nlp_gym/data_pools/custom_seq_tagging_pools.py
@@ -1,4 +1,8 @@
+# standard libaries
+from pathlib import Path
+
 # third party libraries
+import joblib
 from flair import datasets
 from flair.data import Sentence
 from torchnlp.datasets import ud_pos_dataset
@@ -7,6 +11,43 @@ from tqdm import tqdm
 # rlner libraries
 from rlner.nlp_gym.data_pools.base import Sample
 from rlner.nlp_gym.data_pools.multi_label_pool import MultiLabelPool
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+DATA_DIR = ROOT_DIR / "data"
+PREPARED_DIR = DATA_DIR / "prepared"
+NOISE_DIR = PREPARED_DIR / "noise"
+
+
+class Re3dTaggingPool(MultiLabelPool):
+    """Tagging Pool for Re3d Dataset"""
+
+    @classmethod
+    def prepare(cls, split: str):
+        """Prepare Tagging Pool"""
+        sentences = Re3dTaggingPool._get_dataset_from_path(split)
+
+        samples = []
+        all_labels = []
+        for sent in sentences:
+            token_texts = [tok[0] for tok in sent]
+            token_texts_ = " ".join(token_texts)
+
+            # check token to text
+            flair_sentence = Sentence(token_texts_, use_tokenizer=False)
+            assert len(flair_sentence.tokens) == len(token_texts)
+
+            token_labels = [tok[-1] for tok in sent]
+            sample = Sample(input_text=token_texts_, oracle_label=token_labels)
+            all_labels.extend(token_labels)
+            samples.append(sample)
+        weights = [1.0] * len(samples)
+        return cls(samples, list(set(all_labels)), weights)
+
+    @staticmethod
+    def _get_dataset_from_path(split: str):
+        with open(NOISE_DIR / f"noise_{split}.joblib", "rb") as fp:
+            sentences = joblib.load(fp)
+        return sentences
 
 
 class UDPosTagggingPool(MultiLabelPool):
